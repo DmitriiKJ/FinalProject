120147792cb22501bf60e4a20027edb6783a559a
kurbatovdima <kurbatovdima2006@gmail.com>
2023-10-04 09:05:24 +0200

    End 0.1.1 (Add show fired manager)

--- dialogmanager.cpp
+++ dialogmanager.cpp
@@ -181,3 +181,46 @@ void DialogManager::on_pushButton_restore_clicked()
     this->close();
 }
 
+
+void DialogManager::on_pushButton_fired_clicked()
+{
+    ui->tableWidget_manager->setRowCount(0);
+    query->clear();
+    query->exec("SELECT * FROM Manager WHERE isFired is not NULL");
+    int i = 0;
+    while(query->next())
+    {
+        QString first = query->value(1).toString();
+        QString middle = query->value(2).toString();
+        QString last = query->value(3).toString();
+        QDate date = query->value(4).toDate();
+        QString exp = query->value(5).toString();
+        QString phone = query->value(6).toString();
+
+        ui->tableWidget_manager->setRowCount(i + 1);
+
+        QTableWidgetItem* First = new QTableWidgetItem();
+        QTableWidgetItem* Middle = new QTableWidgetItem();
+        QTableWidgetItem* Last = new QTableWidgetItem();
+        QTableWidgetItem* Phone = new QTableWidgetItem();
+        QTableWidgetItem* Birthday = new QTableWidgetItem();
+        QTableWidgetItem* Exp = new QTableWidgetItem();
+
+        First->setText(first);
+        Middle->setText(middle);
+        Last->setText(last);
+        Phone->setText(phone);
+        Birthday->setText(date.toString());
+        Exp->setText(exp);
+
+        ui->tableWidget_manager->setItem(i, 0, First);
+        ui->tableWidget_manager->setItem(i, 1, Middle);
+        ui->tableWidget_manager->setItem(i, 2, Last);
+        ui->tableWidget_manager->setItem(i, 3, Phone);
+        ui->tableWidget_manager->setItem(i, 4, Birthday);
+        ui->tableWidget_manager->setItem(i, 5, Exp);
+
+        i++;
+    }
+}
+
--- dialogmanager.h
+++ dialogmanager.h
@@ -34,6 +34,8 @@ private slots:
 
     void on_pushButton_restore_clicked();
 
+    void on_pushButton_fired_clicked();
+
 private:
     Ui::DialogManager *ui;
     DialogAddManager* addManager;
